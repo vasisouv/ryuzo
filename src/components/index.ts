27ee357e36e6229c0072bef3e179e967804a71ee
vasisouv <souvatzisbill@gmail.com>
2022-10-09 15:54:46 +0300

    improve exports, up version

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import RButton from '~/components/RButton.vue'
+import RSpinner from '~/components/RSpinner.vue'
+
+export { RButton, RSpinner }
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+import { Variant } from '~/types/variant'
+import { Size } from '~/types/size'
+
+export { Variant, Size }
