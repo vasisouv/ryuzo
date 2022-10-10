27ca9a01a4b1dc7d25c45210d7bdb0add7d456d7
vasisouv <souvatzisbill@gmail.com>
2022-10-10 20:50:11 +0300

    manually override vitepress theme css

---
+++ src/components/docs/DocsSectionFlexContainer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="mx-auto flex max-w-3xl flex-col items-center justify-start space-y-4 sm:flex-row sm:items-end sm:justify-around sm:space-y-0"
+  >
+    <slot />
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped></style>
